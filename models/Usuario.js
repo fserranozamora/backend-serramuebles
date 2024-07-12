d747879e6af5e8fda721c745ac9138e9aabbf689
fserranozamora <85131139+fserranozamora@users.noreply.github.com>
2024-07-12 18:25:42 -0500

    Update Usuario.js
    editar Usuario

--- models/Usuario.js
+++ models/Usuario.js
@@ -1,6 +1,6 @@
 const mongoose =require ("mongoose");
 
-const UsuariosSchema =mongoose.Schema({
+const UsuariosSchema = new mongoose.Schema({
     nombre:{type:String, required:true, trim:true},
     email:{type:String, required:true, trim:true, unique:true},
     password :{type:String, required:true, trim:true},
