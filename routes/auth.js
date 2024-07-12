e517635f0a24a797f16a597e4e67d5a2674de96b
fserranozamora <85131139+fserranozamora@users.noreply.github.com>
2024-07-12 18:24:43 -0500

    Update auth.js
    Actualizar register y login

--- routes/auth.js
+++ routes/auth.js
@@ -1,22 +1,33 @@
-const express = require( "express");
+const express = require("express");
 const router = express.Router();
-const { check } = require ("express-validator");
-const authController = require ("../controllers/authcontroller");
-const auth = require ("../middleware/auth");
+const { check } = require("express-validator");
+const authController = require("../controllers/authcontroller");
+const auth = require("../middleware/auth");
 
 //autenticar el usuario ---// api/auth
 
 router.post(
-    "/", [ 
-        check("email", "Digite un correo electrónico válido").isEmail(),
-        check("password", "La contraseña debe tener minimo 10 caracteres").isLength({
-            min: 8,
-        }),  
-    ],
-authController.autenticarusuario
+    "/", [
+    check("email", "Digite un correo electrónico válido").isEmail(),
+    check("password", "La contraseña debe tener minimo 10 caracteres").isLength({
+        min: 8,
+    }),
+],
+    authController.autenticarusuario
 );
 
-router.get("/",auth , authController.autenticarusuario);
+router.post("/register",
+    [
+        check("email", "Digite un correo electrónico válido").isEmail(),
+        check("password", "Contraseña debil").isStrongPassword(),
+        check("nombre", "Mínimo tres carácteres, máximo 40 carácteres").isLength({
+            min: 3,
+            max: 40,
+        }),
+    ],
+    authController.register)
+
+router.get("/", auth, authController.autenticarusuario);
 
 
 module.exports = router;
